1ba76e5343a872d8f683fc1a9bab4177802c9eb2
freedragon <hangondragon@hotmail.com>
2019-11-19 11:45:44 +0900

    Modified return codes based on instance tags.

--- health_probe_handler.py
+++ health_probe_handler.py
@@ -3,23 +3,36 @@
 # simple HTTP server which run a simple page for health probe
 
 from bottle import route, run, redirect, response
+from logconfig import logger
+from configuration import config
+from InstanceMetadata import instanceMetadata
+import requests, json, os
 
-hostname = 'localhost'
+
+hostname = '0.0.0.0'
 hostport = 9000
+instance_status = 200
 
 def writebody():
     body = '<html><head><title>VM Health Check</title></head>'
     body += '<body><h2> ' + hostname + ' is Healthy </h2><ul><h3></body></html>'
     return body
 
-
 @route('/')
 def root():
     return writebody()
 
-
 @route('/health')
 def isHealthy():
-        return writebody()
+    metadata = instanceMetadata().populate()
+    logger.info(metadata)
+
+    if instance_status == 200 and metadata.isPendingDelete():
+        instance_status = 410 # HTTP Status Code 410 (Gone)
+
+    response.status = instance_status
+
+    return writebody()
 
 run(host=hostname, port=hostport)
+
