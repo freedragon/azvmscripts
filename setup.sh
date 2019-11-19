bf145ac6f28cf9265147843c50b7364d68fcf335
Yong Hee Park <hangondragon@hotmail.com>
2019-11-19 23:54:11 +0900

    Same as bedro96's setup.sh
    Copied from his repo

--- setup.sh
+++ setup.sh
@@ -1,34 +1,23 @@
-#!/bin/bash
+##!/bin/bash
 
 # create a folder for custom script and checkout code
 cd ~
-mkdir scripts 
+mkdir scripts
 cd scripts
 
 #install python  Dependencies
-sudo apt update
-sudo apt -y install python3-pip
-pip3 install psutil
-pip3 install bottle
-pip3 install configparser
-
-#Install and configure Telegraf
-# download the package to the VM 
-# wget https://dl.influxdata.com/telegraf/releases/telegraf_1.8.0~rc1-1_amd64.deb 
-# install the package 
-# sudo dpkg -i telegraf_1.8.0~rc1-1_amd64.deb
-
-# generate the new Telegraf config file in the current directory 
-# telegraf --input-filter cpu:mem --output-filter azure_monitor config > azm-telegraf.conf 
-
-# replace the example config with the new generated config 
-# sudo cp azm-telegraf.conf /etc/telegraf/telegraf.conf
+apt update
+apt -y dist-upgrade
+apt update
+apt -y install python3-pip
+pip3 install psutil bottle configparser Flask
 
 #checkout code
 git clone https://github.com/freedragon/azvmscripts.git
 
 #Start health probe job
-cd azvmscripts
+cd terraform_vmss_ag
+chmod +x *.py
 nohup ./health_probe_handler.py & echo $! > health-probe-pid.file &
 
 # Schedule cron jobs
